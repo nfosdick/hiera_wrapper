6b68c4ac33d3347924df9443b59620e82f096d2a
root <root@puppet.lark.com>
2017-09-19 05:43:06 +0000

    minor

--- manifests/init.pp
+++ manifests/init.pp
@@ -33,18 +33,31 @@ class hiera_wrapper{
   }
 
   class { 'hiera':
-    manage_package     => true,
-    puppet_conf_manage => false,
-    datadir_manage     => false,
-    eyaml              => true,
-    eyaml_extension    => 'yaml',
-    provider           => 'puppetserver_gem',
-    master_service     => 'puppetserver',
-    hierarchy          => [
-      'nodes/%{::trusted.certname}',
-      'role/%{::trusted.extensions.pp_role}',
-      'role/%{::role}',
-      'common',
-    ],
+    hiera_version   =>  '5',
+    hiera5_defaults =>  {"datadir" => "data", "data_hash" => "yaml_data"},
+    eyaml           => true,
+    eyaml_extension => 'yaml',
+    provider        => 'puppetserver_gem',
+    master_service  => 'puppetserver',
+    hierarchy       =>  [
+                          {"name" =>  "Virtual yaml", "path"  =>  "virtual/%{::virtual}.yaml"},
+                          {"name" =>  "Nodes yaml", "paths" =>  ['nodes/%{::trusted.certname}.yaml', 'nodes/%{::osfamily}.yaml']},
+                          {"name" =>  "Default yaml file", "path" =>  "common.yaml"},
+                         ],
   }
+#  class { 'hiera':
+#    manage_package     => true,
+#    puppet_conf_manage => false,
+#    datadir_manage     => false,
+#    eyaml              => true,
+#    eyaml_extension    => 'yaml',
+#    provider           => 'puppetserver_gem',
+#    master_service     => 'puppetserver',
+#    hierarchy          => [
+#      'nodes/%{::trusted.certname}',
+#      'role/%{::trusted.extensions.pp_role}',
+#      'role/%{::role}',
+#      'common',
+#    ],
+#  }
 }
